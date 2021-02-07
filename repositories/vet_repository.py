03c0efb36eb1813c41215e1e9f4f89992a4377de
rabisse <rabisse@gmail.com>
2021-02-07 22:36:03 +0000

    did base of vet repository

---
+++ repositories/vet_repository.py
@@ -0,0 +1,54 @@
+from db.run_sql import run_sql
+from models.vet import Vet
+
+def save(vet):
+    sql = "INSERT INTO vets (name), VALUES (%s) RETURNING *"
+    values = [vet.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    vet.id = id
+    return vet
+
+def select_all():
+    vets = []
+    sql = "SELECT * FROM vets"
+    results = run_sql(sql)
+    for row in results:
+        vet = Vet(row['name'], row['earnings'], row['id'])
+        vets.append(vet)
+    return vets
+
+def select(id):
+    vet = None
+    sql = "SELECT * FROM vets WHERE id = %s"
+    value = [id]
+    result = run_sql(sql, value)[0]
+    if result is not None:
+        vet = Vet(result['name'], result['earnings'], result['id'])
+    return vet
+
+def delete_all():
+    sql = "DELETE FROM vets"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM vets WHERE id = %s"
+    value = [id]
+    run_sql(sql, value)
+
+def update(vet):
+    sql = "UPDATE vets SET (name, earnings) = (%s, %s) WHERE id = %s"
+    values = [vet.name, vet.earnings, vet.id]
+    run_sql(sql, values)
+
+def pets(vet):
+    pets = []
+    sql = "SELECT * FROM pets WHERE vet_id = %s"
+    value = [vet.id]
+    results = run_sql(sql, value)
+    for row in results:
+        pet = Pet(row['name'], row['species'], row['breed'], row['dob'], row['owner_id'], row['vet_id'], row['id'])
+        # is this the right way to put the owner into this? Can I still print out the owner name this way?
+        pets.append(pet)
+    return pets
+
